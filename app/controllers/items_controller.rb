f36cb89b5a0f0dcee5ce5e9d4bff739fdd6b0503
Gabriel Brañeiro <gabbraneiro@gmail.com>
2020-01-22 19:54:39 -0300

    Create items_controller.rb

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,30 @@
+class ItemsController < ApplicationController
+    before_action :set_product, only: [:index, :create]
+    before_action :check_quantity, only: [:create]
+
+    # GET /productos/{codigo}/items
+    def index
+      json_response(@product.items)
+    end
+
+    # POST /productos/:codigo/items
+    def create
+      request_params[:quantity].to_i.times { Item.create({product: @product, status: :disponible})}
+    end
+
+    private
+
+        def set_product
+          @product = Product.find_by_code(params[:product_id])
+          json_response(nil, :not_found, errors = {codigo: 'Código de producto no encontrado'}) if @product.nil?
+        end
+
+        # En caso de que el body no tenga un numero valido mayor a 0, se retorna 400.
+        def check_quantity
+          if (request_params[:quantity].to_s.match(/^\d+$/).nil?)
+            json_response(nil, :bad_request, {cantidad: 'Debe enviar un número natural cuantitativo.'})
+          end
+          # Luego se hace un times do sobre Integer request_params[:quantity],
+          # que en caso de ser cero no se hará
+        end
+end
