949bf616eee78b5f523aa87a82ddaebd667c50ea
Gabriel Brañeiro <gabbraneiro@gmail.com>
2020-01-31 18:50:59 -0300

    .

--- app/exceptions/invalid_quantity_exception.rb
+++
@@ -1,7 +0,0 @@
-class InvalidQuantityException < StandardError
-
-  def message(product_id)
-    "La cantidad requerida para el producto con id #{product_id} es inválida"
-  end
-
-end
--- app/exceptions/no_stock_exception.rb
+++
@@ -1,7 +0,0 @@
-class NoStockException < StandardError
-
-  def message(product_id)
-    "No hay suficiente stock del producto con id #{product_id}"
-  end
-
-end
